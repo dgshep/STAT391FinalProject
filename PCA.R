aad9134424f6625bca29848237265511d9ccfdca
Davis Shepherd <dgshep@gmail.com>
2012-05-24 16:04:01 -0700

    Added image processing

--- PCA.R
+++ PCA.R
@@ -9,14 +9,14 @@ getLabels = function(){
 	}
 	labels
 }
-getImageData = function(){
+getImageData = function(r){
 	to.read = file(file.choose(), "rb")
 	magicNumber = readBin(to.read, integer(), endian = "big")
 	records = readBin(to.read, integer(), endian = "big")
 	rows = readBin(to.read, integer(), endian = "big")
 	columns = readBin(to.read, integer(), endian = "big")
 	data = c()
-	for(i in seq(1, 600)){
+	for(i in seq(1, min(r, records))){
 		for(r in seq(1, rows)) {
 			for(c in seq(1, columns)){
 				data = c(data, as.integer(readBin(to.read, raw(), 1)))
@@ -25,4 +25,10 @@ getImageData = function(){
 	}
 	out = matrix(data, ncol = rows*columns)
 	#prcomp(out, scale. = T)
+}
+toImage = function(vec, name){
+    imageMat = matrix(vec, nrow = 28, byrow = T)
+    imageMat = imageMat - min(imageMat)
+    imageMat = 1 - imageMat/(max(imageMat))
+    writeJPEG(imageMat, target = name)
 }
---
+++ data.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+untitled.py
+
+Created by Davis Shepherd on 2012-05-24.
+Copyright (c) 2012 University of Washington. All rights reserved.
+"""
+
+import sys
+import getopt
+import struct
+
+help_message = '''
+The help message goes here.
+'''
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "i:ho:v", ["help", "output="])
+        except getopt.error, msg:
+            raise Usage(msg)
+    
+        # option processing
+        images = -1
+        for option, value in opts:
+            if option == "-v":
+                verbose = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+            if option in ("-o", "--output"):
+                output = value
+            if option == "-i":
+                images = int(value)
+    
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+        
+    imageFile = open("/Users/Davis/Dropbox/Spring12/STAT391/FinalPaper/train-images-idx3-ubyte", "rb");
+    out = open("/Users/Davis/Dropbox/Spring12/STAT391/FinalPaper/train-images-idx3-ubyte_proc", "wb");
+    magicNumber = struct.unpack(">i", imageFile.read(4))[0]
+    records = struct.unpack(">i", imageFile.read(4))[0]
+    rows = struct.unpack(">i", imageFile.read(4))[0]
+    columns = struct.unpack(">i", imageFile.read(4))[0]
+    data = []
+    if images == -1:
+        images = records
+    print images
+    for i in range(images):
+        if(i % (images/5) == 0):
+            print (str(i*100.0/images) + "%")
+        row = ""
+        for j in range(rows*columns):
+            row = row + (" " + str(struct.unpack(">B", imageFile.read(1))[0]))
+        out.write(row + "\n")
+    #out.write(str(data))
+        
+
+
+    
+
+
+if __name__ == "__main__":
+    sys.exit(main())
