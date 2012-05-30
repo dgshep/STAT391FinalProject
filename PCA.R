3d0915139a38ec40587454ee34d0f53a0323a557
Davis Shepherd <dgshep@gmail.com>
2012-05-30 13:06:26 -0700

    Updated with bays learner

--- PCA.R
+++ PCA.R
@@ -20,7 +20,8 @@ getImageData = function(r){
 	to.read = file(file.choose(), "rb")
 	f = scan(to.read)
 	out = matrix(f[1:(28*28*r)], nrow = r, byrow = T)
-	row.names(out) = getLabels(r)
+	#labels = as.character(getLabels(r))
+	
 	out
 	# magicNumber = readBin(to.read, integer(), endian = "big")
 	#  records = readBin(to.read, integer(), endian = "big")
@@ -50,12 +51,38 @@ plotComponents = function(mat, x, y){
     plot(xvec, yvec, pch = "", xlab = c("Principal Component ", x), ylab = c("Principal Component ", y))
     text(xvec, yvec, label = rownames(mat), col = strtoi(rownames(mat), base = 10) + 1)
 }
-analyze = function(dat){
+train = function(x, comp){
+    model = naiveBayes(x[,-(comp + 1):-ncol(x)], x$labels)
+    model
+}
+addLabels = function(dat, lab){x
+    x = data.frame(data = dat, labels = as.character(lab))
+}
+classify = function(model, newDat){
+    pred = predict(model, newDat)
+    cat(sum(newDat$labels==pred)/nrow(newDat))
+    pred
+}
+analyze = function(dat, lab){
    pc = prcomp(dat)
    newdat = pc$x
-   par(mfrow=c(1,2))
-   plotComponents(newdat, 1, 2)
-   plotComponents(dat, 736, 737)
+      par(mfrow=c(1,2))
+         plotComponents(newdat, 1, 2)
+         plotComponents(dat, 736, 737)
    pc
 }
+data = getImageData(10000)
+labels = as.character(getLabels(10000))
+pca = prcomp(data)
+train.labels = labels[1:9900]
+test.labels = labels[9001:10000]
+train.pca = addLabels(pca$x[1:9000, ], train.labels)
+test.pca = addLabels(pca$x[9001:10000, ], test.labels)
+pca.model = train(train.pca, 100)
+pred = classify(pca.model, test.pca)
+
+train.data = addLabels(data[1:9900, ], train.labels)
+test.data = addLabels(data[9001:10000, ], test.labels)
+std.model = train(train.data, 784)
+pred = classify(std.model, test.data)
 
